298d31c8e8e7641bcbe7016aa32a9af42d5f3dba
saojonas <jonas3130@gmail.com>
2023-02-14 21:42:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,582 @@
+const centrosDeCusto = [
+  {
+    descricao: 'COMPOSTAGEM - UNIDADE MACHADO',
+    codigo: '125000'
+  },
+  {
+    descricao: 'COMPOSTAGEM - PROCESSAMENTO DE LEIRAS UNIDADE MACHADO',
+    codigo: '125002'
+  },
+  {
+    descricao: 'COMPOSTAGEM - ETE COMPOSTAGEM UNIDADE MACHADO',
+    codigo: '125031'
+  },
+  {
+    descricao: 'COMPOSTAGEM - FERTIRRIGACAO COMPOSTAGEM UNIDADE MACHADO',
+    codigo: '125033'
+  },
+  {
+    descricao: 'COMPOSTAGEM - RETRO ESCAVADEIRA CULTIVAR UNIDADE MACHADO',
+    codigo: '125034'
+  },
+  {
+    descricao: 'COMPOSTAGEM - REVOLVEDOR CULTIVAR UNIDADE MACHADO',
+    codigo: '125035'
+  },
+  {
+    descricao: 'COMPOSTAGEM - LAVADOR UNIDADE MACHADO',
+    codigo: '125037'
+  },
+  {
+    descricao: 'COMPOSTAGEM - MÃO DE OBRA DIRETA TRATAMENTO UNIDADE MACHADO',
+    codigo: '125038'
+  },
+  {
+    descricao: 'COMPOSTAGEM - CAMINHÃO CULTIVAR UNIDADE MACHADO',
+    codigo: '125039'
+  },
+
+  {
+    descricao: 'COMPOSTAGEM - TRATAMENTO CAIXA DE LIQUIDOS UNIDADE MACHADO',
+    codigo: '125042'
+  },
+
+  {
+    descricao: 'COMPOSTAGEM - ADMINISTRATIVO UNIDADE MACHADO',
+    codigo: '125043'
+  },
+
+  {
+    descricao: 'ABASTECIMENTO TERCEIROS - CULTIVAR',
+    codigo: '125044'
+  },
+
+  {
+    descricao: 'COMPOSTAGEM - MANUTENÇÃO TRATAMENTO',
+    codigo: '125101'
+  },
+
+  {
+    descricao: 'MANUTENÇÃO - COMPOSTAGEM',
+    codigo: '125100'
+  },
+
+  {
+    descricao: 'CULTIVAR - DESCARACTERIZAÇÃO',
+    codigo: '125200'
+  },
+
+  {
+    descricao: 'CULTIVAR - DESCARACTERIZAÇÃO ADMINISTRATIVO',
+    codigo: '125201'
+  },
+
+  {
+    descricao: 'CULTIVAR - DESCARACTERIZAÇÃO MÃO DE OBRA',
+    codigo: '125202'
+  },
+
+  {
+    descricao: 'CULTIVAR - DESCARACTERIZAÇÃO GERAL',
+    codigo: '125203'
+  },
+
+  {
+    descricao: 'CULTIVAR - DESCARACTERIZAÇÃO ESTOQUE/EXPEDIÇÃO',
+    codigo: '125204'
+  },
+
+  {
+    descricao: 'CULTIVAR - DESCARACTERIZAÇÃO LINHA 01',
+    codigo: '125205'
+  },
+
+  {
+    descricao: 'CULTIVAR - DESCARACTERIZAÇÃO LINHA 02',
+    codigo: '125206'
+  },
+
+  {
+    descricao: 'CULTIVAR - DESCARACTERIZAÇÃO FRAGMAQ',
+    codigo: '125207'
+  },
+
+  {
+    descricao: 'CULTIVAR - DESCARACTERIZAÇÃO PRENSA',
+    codigo: '125208'
+  },
+
+  {
+    descricao: 'MANUTENÇÃO - DESCARACTERIZAÇÃO',
+    codigo: '125250'
+  },
+
+  {
+    descricao: 'DESCARACTERIZAÇÃO - MANUTENÇÃO LINHA 1',
+    codigo: '125251'
+  },
+
+  {
+    descricao: 'DESCARACTERIZAÇÃO - MANUTENÇÃO LINHA 2',
+    codigo: '125252'
+  },
+
+  {
+    descricao: 'DESCARACTERIZAÇÃO - MANUTENÇÃO PRENSA',
+    codigo: '125253'
+  },
+
+  {
+    descricao: 'BENEFICIAMENTO/PENEIRAMENTO',
+    codigo: '131000'
+  },
+
+  {
+    descricao: 'PENEIRA MALHA 12',
+    codigo: '131010'
+  },
+
+  {
+    descricao: 'SECADOR MALHA 6',
+    codigo: '131030'
+  },
+
+  {
+    descricao: 'ARMAZENAMENTO EXPEDIÇÃO',
+    codigo: '131040'
+  },
+
+  {
+    descricao: 'MÃO DE OBRA DIRETA BENEFICIAMENTO',
+    codigo: '131060'
+  },
+
+  {
+    descricao: 'BENEFICIAMENTO - ENERGIA ELETRICA',
+    codigo: '131070'
+  },
+
+  {
+    descricao: 'BENEFICIAMENTO GERAL',
+    codigo: '131090'
+  },
+
+  {
+    descricao: 'MANUTENÇÃO - BENEFICIAMENTO/PENEIRAMENTO',
+    codigo: '131200'
+  },
+
+  {
+    descricao: 'MANUTENÇÃO - PENEIRA MALHA 12',
+    codigo: '131201'
+  },
+
+  {
+    descricao: 'MANUTENÇÃO - SECADOR MALHA 6',
+    codigo: '131202'
+  },
+
+  {
+    descricao: 'FOLIAR FABRICA',
+    codigo: '132000'
+  },
+
+  {
+    descricao: 'FOLIAR- LINHA DE SAIS',
+    codigo: '132010'
+  },
+
+  {
+    descricao: 'FOLIAR- LINHA DE FOLIAR LIQUIDOS',
+    codigo: '132020'
+  },
+
+  {
+    descricao: 'FOLIAR- LINHA DE ENVASE (BALANCA E SELADORA)',
+    codigo: '132030'
+  },
+
+  {
+    descricao: 'FOLIAR- GASTOS GERAIS FOLIAR',
+    codigo: '132090'
+  },
+
+
+    
+
+
+
+
+
+
+
+
+
+  {
+    descricao: 'MANUTENÇÃO - FOLIAR',
+    codigo: '132200'
+  },
+
+  {
+    descricao: 'MANUTENÇÃO - FABRICA FOLIAR ',
+    codigo: '132201'
+  },
+
+  {
+    descricao: 'FÁBRICA FERTILIZANTE ORGANOMINERAL',
+    codigo: '133000'
+  },
+
+  {
+    descricao: 'MOD LINHA 01 PELETIZACAO',
+    codigo: '133001'
+  },
+
+
+
+  {
+    descricao: 'MOD MISTURADOR,MOEGA E MOAGEM L1',
+    codigo: '133003'
+  },
+
+  {
+    descricao: 'GGF - FABRICA PELETIZADO- MISTURADOR,MOEGA E MOAGEM (FASE 1 PRODUCAO) L-1',
+    codigo: '133004'
+  },
+
+  {
+    descricao: 'GGF- FABRICA PELETIZADO- PELETIZADORAS LINHA 01',
+    codigo: '133005'
+  },
+
+  {
+    descricao: 'MOD FABRICA PELETIZADO- PELETIZADORAS LINHA 01',
+    codigo: '133006'
+  },
+
+  {
+    descricao: 'MOD ENVASE E CARREGAMENTO LINHA 01',
+    codigo: '133007'
+  },
+
+  {
+    descricao: 'GGF - FABRICA PELETIZADO - ENVASE E CARREGAMENTO L1 PELETIZACAO',
+    codigo: '133008'
+  },
+
+  {
+    descricao: 'MOD- FABRICA FARELADO - MISTURADOR,MOEGA E MOAGEM L2',
+    codigo: '133009'
+  },
+
+  {
+    descricao: 'GGF - FABRICA FARELADO - MISTURADOR,MOEGA E MOAGEM L2',
+    codigo: '133012'
+  },
+
+  {
+    descricao: 'MOD ENVASE E CARREGAMENTO LINHA 02',
+    codigo: '133013'
+  },
+
+  {
+    descricao: 'GGF - FABRICA FARELADO-ENVASE E CARREGAMENTO L2',
+    codigo: '133014'
+  },
+
+  {
+    descricao: 'FABRICA GERAL - GASTOS GERAIS DE FABRICAÇÃO',
+    codigo: '133015'
+  },
+
+  {
+    descricao: 'FABRICA - DESCARGA, MOVIMENTAÇÃO E ARMAZENAGEM',
+    codigo: '133016'
+  },
+
+  {
+    descricao: 'FABRICA - LIMPEZA E LUBRIFICAÇÃO ÁREA PRODUTIVA',
+    codigo: '133017'
+  },
+
+  
+  {
+    descricao: 'FABRICA-CABINE DE AUTOMAÇÃO',
+    codigo: '133018'
+  },
+
+  {
+    descricao: 'FABRICA-COMPRESSORES E GERADORES',
+    codigo: '133019'
+  },
+
+  {
+    descricao: 'FABRICA-MANUTENCAO FABRICA DE FERTILIZANTES',
+    codigo: '133020'
+  },
+
+  {
+    descricao: 'FABRICA - ENERGIA ELETRICA',
+    codigo: '133021'
+  },
+
+  {
+    descricao: 'FABRICA - REFEITORIO E LAVANDERIA',
+    codigo: '133022'
+  },
+
+  {
+    descricao: 'FABRICA - GERENCIA DA PRODUÇÃO',
+    codigo: '133023'
+  },
+
+
+
+
+  {
+    descricao: 'MATÉRIA - PRIMA FERTILIZANTES',
+    codigo: '133025'
+  },
+
+  {
+    descricao: 'FABRICA - DIRETORIA FABRIL',
+    codigo: '133026'
+  },
+
+  {
+    descricao: 'MANUTENÇÃO - FABRICA FERTILIZANTES ORGANOMINERAL',
+    codigo: '133200'
+  },
+
+  {
+    descricao: 'MANUTENÇÃO - FABRICA LINHA 1',
+    codigo: '133201'
+  },
+
+  {
+    descricao: 'MANUTENÇÃO - FABRICA  LINHA 2	',
+    codigo: '133202'
+  },
+
+  {
+    descricao: 'INVESTIMENTO - ROTATIVO INDUSTRIAL',
+    codigo: '300199'
+  },
+
+  {
+    descricao: 'INVESTIMENTO - TERRAPLANAGEM',
+    codigo: '300200'
+  },
+
+  {
+    descricao: 'INVESTIMENTO - FABRICAÇÃO E MANUTENÇÃO DE EQUIPAMENTOS',
+    codigo: '300201'
+  },
+
+   {
+    descricao: 'INVESTIMENTO - BASE CIVIL',
+    codigo: '300202'
+  },
+
+  {
+    descricao: 'INVESTIMENTO - INFRAESTRUTURA ELÉTRICA E CCM',
+    codigo: '300203'
+  },
+
+  {
+    descricao: 'INVESTIMENTO - GRUPO GERADOR',
+    codigo: '300204'
+  },
+
+   {
+    descricao: 'INVESTIMENTO - GALPÃO PRÉ-MOLDADO',
+    codigo: '300205'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - ESTUFA DE COMPOSTO',
+    codigo: '300206'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - AUTOMAÇÃO DO SETOR DE PELETIZAÇÃO',
+    codigo: '300174'
+  },
+  {
+    descricao: 'CAPEX 2022 - REFORMA DO SISTEMA DE RESFRIAMENTO E SUBSTITUIÇÃO DE CICLONES',
+    codigo: '300171'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - SUBSTITUIÇÃO DE CONJUNTOS DE TRAÇÃO DOS TRANSPORTADORES (LINHA 1)',
+    codigo: '300172'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - REFORMA DOS HELICÓIDES DO MISTURADOR (MT1101)',
+    codigo: '300175'
+  },
+
+   {
+    descricao: 'CAPEX 2022 - PROJETO DE ELÉTRICA E AUTOMAÇÃO - 1º ETAPA (LINHA 1)',
+    codigo: '300173'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - TROCA DO SISTEMA DE ALIMENTAÇÃO DAS PELETIZADORAS',
+    codigo: '300176'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - SUBSTITUIÇÃO DE CONJUNTOS DE TRAÇÃO DOS TRANSPORTADORES (LINHA 2)',
+    codigo: '300177'
+  },
+
+   {
+    descricao: 'CAPEX 2022 - PROJETO DE ELÉTRICA E AUTOMAÇÃO - 1º ETAPA (LINHA 2)',
+    codigo: '300178'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - SUBSTITUIÇÃO DE SISTEMA DE ENVASE DE BIGBAG',
+    codigo: '300179'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - PROJETO E SUBSTITUIÇÃO DE BANCO DE CAPACITORES',
+    codigo: '300164'
+  },
+  {
+    descricao: 'CAPEX 2022 - SUBSTITUIÇÃO DA TUBULAÇÃO PNEUMÁTICA',
+    codigo: '300165'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - AVCB E SPDA',
+    codigo: '300168'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - TERRAPLANAGEM E INSTALAÇÃO DA BALANÇA RODOVIÁRIA E APOIO BALANÇA',
+    codigo: '300166'
+  },
+
+   {
+    descricao: 'CAPEX 2022 - AMPLIAÇÃO DOS BOX DE MATÉRIA PRIMA',
+    codigo: '300167'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - INSTALAÇÃO DE CANCELAS DA PORTARIA',
+    codigo: '300170'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - INSTALAÇÃO DE SISTEMA DE MONITORAMENTO POR CÂMERAS',
+    codigo: '300187'
+  },
+
+   {
+    descricao: 'CAPEX 2022 - CONSTRUÇÃO E MELHORIA DE REFEITÓRIO',
+    codigo: '300163'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - AMPLIAÇÃO DO BARRACÃO DO PENEIRAMENTO',
+    codigo: '300181'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - ADEQUAÇÃO DE PAINEL ELÉTRICO',
+    codigo: '300182'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - SUBSTITUIÇÃO DO CICLONE DO SECADOR I E ROTOR DO EXAUSTOR',
+    codigo: '300183'
+  },
+
+  {
+    descricao: 'CAPEX 2022 - REFORMA E MELHORIA NO SISTEMA DE DOSAGEM',
+    codigo: '300180'
+  },
+
+   {
+    descricao: 'CAPEX 2022 - MANUTENÇÃO CORRETIVA E PREVENTIVA NAS MÁQUINAS PELETIZADORAS',
+    codigo: '300187'
+  }
+
+];
+
+function buscarCentroDeCustoPelaDescricao(pedacoDescricao) {
+  let resultados = [];
+
+  for (const centroDeCusto of centrosDeCusto) {
+    if (centroDeCusto.descricao.toLowerCase().includes(pedacoDescricao.toLowerCase())) {
+      resultados.push(centroDeCusto);
+    }
+  }
+
+  return resultados;
+}
+
+
+
+
+
+console.log(centrosDeCusto)
+
+
+
+function mostrarResultados() {
+  const descricao = document.getElementById('descricao').value;
+  const resultados = buscarCentroDeCustoPelaDescricao(descricao);
+
+  // obter a referência do elemento que contém a tabela
+  const tableContainer = document.querySelector('#table-container');
+
+  // remover a tabela existente, se houver
+  if (tableContainer.querySelector('table')) {
+    tableContainer.removeChild(tableContainer.querySelector('table'));
+  }
+
+  // criar a tabela e adicionar ao container
+  const table = document.createElement('table');
+  tableContainer.appendChild(table);
+
+  // adicionar uma classe para remover as bordas da tabela
+  //table.classList.add('border');
+
+  // criar o cabeçalho da tabela
+  const thead = document.createElement('thead');
+  table.appendChild(thead);
+  const tr = document.createElement('tr');
+  thead.appendChild(tr);
+  const thCC = document.createElement('th');
+  thCC.textContent = 'CC';
+  tr.appendChild(thCC);
+  const thDescricao = document.createElement('th');
+  thDescricao.textContent = 'Descrição';
+  tr.appendChild(thDescricao);
+
+  // criar o corpo da tabela e adicionar as linhas com os dados
+  const tbody = document.createElement('tbody');
+  table.appendChild(tbody);
+
+  for (const resultado of resultados) {
+    const tr = document.createElement('tr');
+    tbody.appendChild(tr);
+
+    const tdCC = document.createElement('td');
+    tdCC.textContent = resultado.codigo;
+    tr.appendChild(tdCC);
+
+    const tdDescricao = document.createElement('td');
+    thDescricao.style.textAlign = 'left';
+    thDescricao.style.paddingLeft = '10px';
+    tdDescricao.style.textAlign = 'left';
+    tdDescricao.style.paddingLeft = '10px';
+    tdDescricao.textContent = resultado.descricao;
+    tr.appendChild(tdDescricao);
+  }
+}
